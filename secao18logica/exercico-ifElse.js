c721d38e6c59a0952bcf37e4d13f963af8453bf0
Danryt <danielssouza91@gmail.com>
2022-11-02 18:41:40 -0300

    Fiz besteira, adicionando tudo novamente

---
+++ secao18logica/exercico-ifElse.js
@@ -0,0 +1,12 @@
+console.log(let tempo = parseFloat(prompt("Digite a quantos anos você está atuando na árae de programção ")));
+
+if (tempo <= 2) {
+    console.log('Acho que você ainda é Júnior, continue firme na sua jornada!!')
+} if (tempo >= 2 && tempo <= 4) {
+    console.log('Olha só, eu diria que você é Pleno, está quase lá!')
+    
+} else {
+    console.log('Nice, temos um Sênior aqui, me ensine todo o seu conhecimento, Ó grande sábio!')
+    
+}
+
